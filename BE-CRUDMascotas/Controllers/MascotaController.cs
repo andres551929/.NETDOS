6e9baffd87382f53a7ef4aee5a3770489005dfbc
Andres <andrespaez123@hotmail.com>
2023-08-06 14:32:46 -0500

    commit

---
+++ BE-CRUDMascotas/Controllers/MascotaController.cs
@@ -0,0 +1,35 @@
+﻿using BE_CRUDMascotas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BE_CRUDMascotas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MascotaController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+        public MascotaController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var ListMascotas = await _context.Mascotas.ToListAsync();
+                //var ListMascotas = _context.Mascotas.ToList();
+                return Ok(ListMascotas);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
--- BE-CRUDMascotas/Program.cs
+++ BE-CRUDMascotas/Program.cs
@@ -1,3 +1,7 @@
+using BE_CRUDMascotas.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +11,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => options.AddPolicy("AllowWebApp",
+    builder => builder.AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod()));
+//agregar context
+builder.Services.AddDbContext<AplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conexion"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowWebApp");
 
 app.UseHttpsRedirection();
 
