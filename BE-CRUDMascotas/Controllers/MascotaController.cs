6b64a8a734b9806690c5af71450c1760d49a8c92
Andres <andrespaez123@hotmail.com>
2023-08-07 13:52:39 -0500

    commit

--- BE-CRUDMascotas/Controllers/MascotaController.cs
+++ BE-CRUDMascotas/Controllers/MascotaController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 
 namespace BE_CRUDMascotas.Controllers
 {
@@ -27,6 +28,99 @@ namespace BE_CRUDMascotas.Controllers
             catch (Exception ex)
             {
 
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var mascota = await _context.Mascotas.FindAsync(id);
+                if (mascota == null)
+                {
+                    return NotFound();
+                }
+                return Ok(mascota);
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var mascota = await _context.Mascotas.FindAsync(id);
+                if (mascota == null)
+                {
+                    return NotFound();
+                }
+                _context.Mascotas.Remove(mascota);    
+               await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public async Task <IActionResult> Post(Mascota mascota)
+        {
+            try
+            {
+                mascota.FechaCreacion = DateAndTime.Now;
+                _context.Add(mascota);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("Get", new {id = mascota.Id}, mascota);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Mascota mascota)
+        {
+
+            try
+            {
+                if (id! != mascota.Id )
+                {
+                    return BadRequest();
+                }
+                //_context.Update(mascota);                
+                var mascotaItem = await _context.Mascotas.FindAsync(id);
+
+                if (mascotaItem == null)
+                {
+                    return NotFound();  
+                }
+                mascotaItem.Nombre = mascota.Nombre;
+                mascotaItem.Raza = mascota.Raza;
+                mascotaItem.Color = mascota.Color;
+                mascotaItem.Edad = mascota.Edad;
+                mascotaItem.Peso = mascota.Peso;    
+
+                await _context.SaveChangesAsync();  
+                return NoContent(); 
+            }
+            catch (Exception ex)
+            {
                 return BadRequest(ex.Message);
             }
         }
